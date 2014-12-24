0517afe15604f0853c776e91ad6b07ca11be7b3a
Dimitar Chaushev <d.chaushev073@gmail.com>
2014-12-24 12:52:15 +0200

    minor edits

--- src/com/fmi/corejava/io/MatrixFileInteraction.java
+++ src/com/fmi/corejava/io/MatrixFileInteraction.java
@@ -12,7 +12,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
